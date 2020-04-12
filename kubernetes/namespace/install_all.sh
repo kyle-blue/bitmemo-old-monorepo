87c2e61dbaff76f33fd8d02ccf801d6ae2ae9d6e
Kyle Doidge <kyle.blue.nuttall.09@gmail.com>
2020-04-12 21:02:37 +0100

    Added kubernetes install-all shell script

---
+++ kubernetes/namespace/install_all.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "Have you created kubeadm node yet? (y/n)"
+read CREATED
+
+if [ "$CREATED" != "y" ]; then
+    sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+
+    mkdir -p $HOME/.kube
+    sudo echo yes | sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown $(id -u):$(id -g) $HOME/.kube/config
+fi
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+kubectl create namespace build
+kubectl create namespace app
+kubectl create namespace argocd
+kubectl apply -Rf $SCRIPTPATH
+kubectl taint nodes --all node-role.kubernetes.io/master- # This is for calico setup
