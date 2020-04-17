f878e69ebe7b5eb1daf40f52d36f694c60221f97
Kyle Doidge <kyle.blue.nuttall.09@gmail.com>
2020-04-17 16:28:02 +0100

    Added registry creds as a secret

--- docker/cert-renewal-image/entrypoint.sh
+++ docker/cert-renewal-image/entrypoint.sh
@@ -3,8 +3,8 @@
 # $1 should be domain name
 
 # Change test-cert when ready
-printf "a\ny\n1" | certbot certonly --verbose --standalone --test-cert -m "kyle.blue.nuttall@gmail.com" -d "$1", # Change "bitmemo.io" if using other website
-cd "/etc/letsencrypt/live/$1/";,
+printf "a\ny\n1" | certbot certonly --verbose --standalone --test-cert -m "kyle.blue.nuttall@gmail.com" -d "$WEBSITE", # Change "bitmemo.io" if using other website
+cd "/etc/letsencrypt/live/$WEBSITE/";,
 sed -i '' **/*;, # This replaces all system links with their actual files
 
 cd /create_secrets
